6a81e6063b4419061d53fb54020c0f9fa33eb9c5
jason <jasonedlewis@gmail.com>
2019-10-02 11:45:52 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
+import Form from "./components/Form";
+
 
 class App extends Component {
+
+
+  state ={
+      allBooks:[],
+      bookShelf:[]
+
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3005/books')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        allBooks: data 
+      })
+    })
+
+  }
+
+addToShelf =(id)=>{
+  // console.log("This is ADD", id)
+  const chosenBook = this.state.allBooks.find(book => book.id === id)
+  if( !this.state.bookShelf.includes(chosenBook)) {this.setState({
+    bookShelf: [chosenBook, ...this.state.bookShelf]
+  }) }
+
+
+}
+
+removeFromShelf =(id)=>{
+ const remainingBooks = this.state.bookShelf.filter(book=> book.id !==id)
+
+ this.setState({
+  bookShelf: remainingBooks
+ })
+
+}
+
+addNewBook =(state)=>{
+  console.log("From App", state)
+  }
+
+
   render() {
+
+    // console.log("All books state",this.state.allBooks)
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList books={this.state.allBooks} addToShelf={this.addToShelf}/>
+        <Bookshelf book={this.state.bookShelf} removeFromShelf={this.removeFromShelf} />
+        <Form addBook={this.addNewBook}/>
       </div>
     );
   }
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,12 +3,23 @@ import Book from "../components/Book";
 import Form from "../components/Form";
 
 class BookList extends Component {
+  displayBooks = () => {
+    return this.props.books.map(book => (
+      <Book
+        book={book}
+        key={book.id}
+        handleClick={() => this.props.addToShelf(book.id)}
+      />
+    ));
+  };
+
   render() {
+    // console.log("Book list props",this.props)
     return (
       <div className="book-list">
         <h1>Book List</h1>
         <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <ul>{this.displayBooks()}</ul>
       </div>
     );
   }
