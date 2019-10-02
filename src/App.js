ccb22ee7141dc1409dfb414f6991164230be42a2
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-10-02 10:39:41 -0400

    ,

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+
   render() {
     return (
       <div className="book-container">
@@ -13,6 +21,10 @@ class App extends Component {
       </div>
     );
   }
+  componentDidMount() {
+    fetch('http://localhost:3005/books')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
 }
-
+}
 export default App;
