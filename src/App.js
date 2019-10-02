8bf546495bf856f4bd022a9d47687f7ffec663e4
github username <github email address>
2019-10-02 11:45:25 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+  state = {
+    allBooks: [],
+    bookShelf: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3005/books')
+    .then(response => response.json())
+    .then(data => this.setState({
+      allBooks: data
+    }))
+  }
+
+  addToBookShelf = (bookObj) => {
+    const allbooks = this.state.allBooks
+
+    const thebook = allbooks.find(book => {
+      if (book === bookObj){
+        book.inShelf = true;
+
+        return book
+      }
+    })
+
+    this.setState({
+      bookShelf: [...this.state.bookShelf, thebook]
+    })
+  }
+
+  removeFromBookShelf = (book) => {
+
+    let bookShelf = this.state.bookShelf
+
+    let updatedBookShelf = bookShelf.filter(shelfBook => {
+      return shelfBook !== book
+    })
+
+    this.setState({
+      bookShelf: updatedBookShelf
+    })
+  }
+
+  handler = (book) =>{
+    if (book.inShelf === true && this.state.bookShelf.includes(book)) {
+      this.removeFromBookShelf(book)
+    } else if (this.state.bookShelf.includes(book)) {
+      console.log("CAN'T HAVE DUPLICATE BOOKS")
+    } else {
+      this.addToBookShelf(book)
+    }
+  }
+
   render() {
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList bookData={this.state.allBooks} addToBookShelf={this.addToBookShelf} handler={this.handler}/>
+        <Bookshelf bookData = {this.state.bookShelf} handler={this.handler}/>
       </div>
     );
   }
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,12 +3,18 @@ import Book from "../components/Book";
 import Form from "../components/Form";
 
 class BookList extends Component {
+
+
+
+
   render() {
     return (
       <div className="book-list">
         <h1>Book List</h1>
         <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <ul>{this.props.bookData.map(book => {
+          return <li><Book bookData={book} handler={this.props.handler}/></li>
+        })}</ul>
       </div>
     );
   }
