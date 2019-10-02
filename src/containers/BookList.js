3c0a361a98c810dc541aa1e7f2aaa87d5f1247f3
CihatAld <cihataldanmaz@gmail.com>
2019-10-02 11:40:03 -0400

    done

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 import Book from "../components/Book";
 import Form from "../components/Form";
+import Bookshelf from "./Bookshelf";
 
 class BookList extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      bookclicked : []
+    }
+  }
+
+  handleClick = (arg) => {
+    this.setState({bookclicked : arg})
+  }
+
   render() {
     return (
       <div className="book-list">
         <h1>Book List</h1>
         <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <ul>{this.props.books.map((book,i) => 
+              <Book book={book} key={i} handleClick={this.handleClick}/>
+              
+          )}
+          
+          </ul>
+          <Bookshelf book = {this.state.bookclicked}/>
       </div>
     );
   }
